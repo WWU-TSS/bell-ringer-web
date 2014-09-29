c6ac0fd8efac0bf7ffb373d198ad700d0ac3ddd8
programmerjake <programmerjake@gmail.com>
2014-09-29 15:05:51 -0700

    added create-archive.sh

---
+++ create-archive.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tar -czf bell-ringer-web.tar.gz bell-ringer-administrate.c bell-ringer-administrate.sh bell-ringer.cgi bell-ringer.cpp change-password.cgi clean.sh install.sh index.html parallel_port.cpp parallel_port.cpp start-bell-ringer.sh stop-bell-ringer.sh
