c8b7ca47b576cb0ecf3becb5069a045511d33426
programmerjake <programmerjake@gmail.com>
2014-09-29 19:47:11 -0700

    fixed bug in make-user.sh

--- make-user.sh
+++ make-user.sh
@@ -16,26 +16,23 @@ elif [ "`whoami`" != "root" ]; then
     exit 1
 fi
 for username in "$@"; do
-    echo "$username"
-    adduser --disabled-password "$username" || exit 1
+    adduser --disabled-password "$username"
     for i in adm dialout sudo audio video plugdev games users netdev input spi gpio; do
-        adduser "$username" "$i"
+        adduser "$username" "$i" &> /dev/null
     done
-    password="`tr -dc [:alnum:] < /dev/urandom | head -c 16`"
+    password="`tr -dc [:alnum:] 2> /dev/null < /dev/urandom | head -c 16`"
     printf "%s\n" "$username:$password" | chpasswd
     email_file="new-password-email-for-$username.txt"
-    
     touch "$email_file"
     chmod 600 "$email_file"
     if [ "a$SUDO_USER" != "a" ]; then
-    chown "$SUDO_USER:$SUDO_USER" "$email_file"
-    cat > "$email_file" <<EOF
-Your new username and password for the Administration Building's bell are:
-Username: $username
-Password: $password
-
-$access_msg
-EOF
-    echo "email contents of $email_file to $username" >&2
+        chown "$SUDO_USER:$SUDO_USER" "$email_file"
+    fi
+    echo "Your new username and password for the Administration Building's bell are:" > "$email_file"
+    echo "Username: $username" >> "$email_file"
+    echo "Password: $password" >> "$email_file"
+    echo >> "$email_file"
+    echo "$access_msg" >> "$email_file"
+    echo "email contents of $email_file to $username"
 done
 
